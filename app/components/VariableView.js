63582ff1eef7bc7d89e81df1d7d152436b446107
Almenon <Almenon@users.noreply.github.com>
2018-06-13 22:00:02 -0700

    adding variable view mock

---
+++ app/components/VariableView.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'React'
+
+export default class VariableView extends Component<Props>{
+    props: Props;
+
+    render(){
+        return <div style={{width:'200px',height:'200px'}}>var preview here</div>
+    }
+}
