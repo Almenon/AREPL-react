2f882013fbc88ce2480c675f75c03eeaa1701017
Almenon <Almenon@users.noreply.github.com>
2018-06-24 09:48:49 -0700

    fixing import error (no default)

--- src/Renderer/routes.tsx
+++ src/Renderer/routes.tsx
@@ -1,5 +1,5 @@
 /* eslint flowtype-errors/show-errors: 0 */
-import React from 'react';
+import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
