938fa218914af81bc730873cdb333372ea7cb68f
Almenon <Almenon@users.noreply.github.com>
2018-06-24 10:02:35 -0700

    apparently i cant import styles anymore :(

--- src/Renderer/components/Home.tsx
+++ src/Renderer/components/Home.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 // import { Link } from 'react-router-dom';
 import PyInput from './PyInput'
 import Output from './Output'
-import styles from './Home.css';
+const styles  = require('./Home.css');
 import Editor from './Editor';
 
 type Props = {
