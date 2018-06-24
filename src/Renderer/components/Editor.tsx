90fc068bf7c492a233406614531fa22646cccf58
Almenon <Almenon@users.noreply.github.com>
2018-06-24 11:03:33 -0700

    fixed props

--- src/Renderer/components/Editor.tsx
+++ src/Renderer/components/Editor.tsx
@@ -5,7 +5,6 @@ type Props = {
 }
 
 export default class Editor extends React.Component<Props>{
-    props: Props;
 
     handleChange = (event:any) => {
         const text:string = event.target.value
@@ -13,6 +12,6 @@ export default class Editor extends React.Component<Props>{
     }
 
     render(){
-        return <textarea style={{width:'400px',height:'800px',backgroundColor:'#d6d1d1'}} onChange={this.handleChange}></textarea>
+        return <textarea style={{width:'400px',height:'800px',backgroundColor:'#d6d1d1'}} onChange={this.handleChange}/>
     }
 }
