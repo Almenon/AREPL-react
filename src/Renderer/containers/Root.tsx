1dd6288c831d58ec8505189ee0f7da130862ee53
Almenon <Almenon@users.noreply.github.com>
2018-06-24 09:53:21 -0700

    updating root container from typescript boilerplate repo

--- src/Renderer/containers/Root.tsx
+++ src/Renderer/containers/Root.tsx
@@ -1,15 +1,16 @@
-// @flow
-import React, { Component } from 'react';
+import * as React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import { Store } from 'redux';
+import { History } from 'history';
 import Routes from '../routes';
 
-type Props = {
-  store: {},
-  history: {}
-};
+interface Props {
+  store: Store<any>;
+  history: History;
+}
 
-export default class Root extends Component<Props> {
+export default class Root extends React.Component<Props> {
   render() {
     return (
       <Provider store={this.props.store}>
