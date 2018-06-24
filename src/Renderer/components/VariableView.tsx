bf1923dd9d92c5caef2c68ca709a9d201d657c92
Almenon <Almenon@users.noreply.github.com>
2018-06-24 11:04:48 -0700

    simplified class into function

--- src/Renderer/components/VariableView.tsx
+++ src/Renderer/components/VariableView.tsx
@@ -1,9 +1,11 @@
 import * as React from 'React'
 
-export default class VariableView extends React.Component<Props>{
-    props: Props;
-
-    render(){
-        return <div style={{width:'200px',height:'200px'}}>{JSON.stringify(this.props.vars)}</div>
-    }
+interface Props{
+    vars:Object
 }
+
+function VariableView({vars}:Props){
+    return <div style={{width:'200px',height:'200px'}}>{JSON.stringify(vars)}</div>
+}
+
+export default VariableView
