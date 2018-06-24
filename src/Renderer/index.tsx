66595276086b2bcaef7667d45577af9c5f27fe7a
Almenon <Almenon@users.noreply.github.com>
2018-06-24 09:47:59 -0700

    updating index from typescript boilerplate repo

--- src/Renderer/index.tsx
+++ src/Renderer/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
+import prodStore from './store/configureStore.prod';
+import devStore from './store/configureStore.dev';
 import './app.global.css';
+
+const configureStore =
+  process.env.NODE_ENV === 'production'
+    ? prodStore.configureStore
+    : devStore.configureStore;
+
+const history =
+  process.env.NODE_ENV === 'production' ? prodStore.history : devStore.history;
 
 const store = configureStore();
 
@@ -16,7 +25,7 @@ render(
 
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
-    const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
+    const NextRoot = require('./containers/Root');
     render(
       <AppContainer>
         <NextRoot store={store} history={history} />
