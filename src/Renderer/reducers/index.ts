937ef7ef796beb5e80d39706b847763f577c5481
Almenon <Almenon@users.noreply.github.com>
2018-06-30 10:06:06 -0700

    adding arepl to reducers

--- src/Renderer/reducers/index.ts
+++ src/Renderer/reducers/index.ts
@@ -1,13 +1,16 @@
 import { combineReducers } from 'redux';
 import { routerReducer, RouterState } from 'react-router-redux';
 import { counterReducer, counterState } from '@red/counter';
+import areplReducer, { areplState } from '@red/arepl';
 
 export interface RootState {
   counter: counterState;
   router: RouterState;
+  arepl: areplState
 }
 
 export const rootReducer = combineReducers({
   counter: counterReducer,
-  router: routerReducer
+  router: routerReducer,
+  arepl: areplReducer
 });
