37e6887b78336c54ee09e9eb6a94545e64c72e10
Almenon <Almenon@users.noreply.github.com>
2018-06-30 10:05:53 -0700

    updating state

--- src/Renderer/reducers/arepl.ts
+++ src/Renderer/reducers/arepl.ts
@@ -1,16 +1,19 @@
-// @flow
 import { PRINT_RESULT, EXECUTION_COMPLETE } from '../actions/arepl';
 
 interface actionType {
   type: string
 };
 
-export default function arepl(state: any, action: actionType) {
+export interface areplState {ERROR:string, userVariables:Object, execTime:number, totalPyTime:number, totalTime:number}
+
+const initialState:areplState = {ERROR: '', userVariables: {}, execTime:0,totalPyTime: 0, totalTime: 0}
+
+export default function areplReducer(state: areplState = initialState, action: actionType) {
   switch (action.type) {
     case PRINT_RESULT:
-      return state;
+      return {...state, ...action};
     case EXECUTION_COMPLETE:
-      return state;
+      return {...state, ...action};
     default:
       return state;
   }
